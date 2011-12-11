fefd423e700462d5216b1b57bed9f6c94f9bd8c5
mathfuru <mathfuru@gmail.com>
2011-12-11 12:05:45 +0900

    CSV2HASH.accumulate_to_hashを実装。

--- lib/csv2hash/csv2hash.rb
+++ lib/csv2hash/csv2hash.rb
@@ -2,10 +2,9 @@ require "csv2hash/sheet"
 
 class CSV2Hash
   def foo
-    Sheet.new(csv_fname).value_area.cells.each do |cell|
-      cell.primary_keys #=> まだない
-      cell.secondary_keys #=> まだない
-      cell.value
+    Sheet.new(csv_fname).value_area.cells.inject({}) do |result, cell|
+      keys = cell.primary_keys + cell.secondary_keys
+      accumulate_to_hash(result, keys, cell.value)
     end
   end
 end
--- lib/csv2hash/helper.rb
+++ lib/csv2hash/helper.rb
@@ -17,3 +17,20 @@ class NilClass
     false
   end
 end
+
+class CSV2Hash
+  def self.accumulate_to_hash(hash, keys, value)
+    result = hash.clone
+    base = result
+    keys.each_with_index do |k, i|
+      if i == keys.size - 1
+        raise "キーの上書きが発生しました" if result[k]
+        result[k] = value
+      else
+        result[k] ||= {}
+        result = result[k]
+      end
+    end
+    base
+  end
+end
